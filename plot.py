d9c4f5a8ed2bb9533ab128c591d26a41bb685432
TheZino <TheZino@github.com>
2017-10-10 13:44:43 +0200

    First upload

---
+++ plot.py
@@ -0,0 +1,74 @@
+import argparse
+import csv
+import matplotlib.pyplot as plt
+
+# Plot settings
+parser = argparse.ArgumentParser(description='Neural Networks data plotter')
+parser.add_argument('-f', '--file', type=str, default='./logs/log.csv',
+                    help='input file to plot (default:./logs/log.csv)')
+args = parser.parse_args()
+
+file = args.file
+vgg_loss = []
+mse_loss = []
+psnr = []
+ssim = []
+
+
+with open(file) as f:
+    reader = csv.reader(f)
+    for row in reader:
+        #print(row)
+        vgg_loss.append(row[1])
+        mse_loss.append(row[2])
+
+        psnr.append(row[3])
+        ssim.append(row[4])
+
+loss_min_value = min(vgg_loss)
+
+loss_min = [loss_min_value] * len(vgg_loss)
+
+psnr_max_value = max(psnr)
+
+psnr_max = [psnr_max_value] * len(psnr)
+
+ssim_max_value = max(ssim)
+
+ssim_max = [ssim_max_value] * len(ssim)
+
+index_psnr = psnr.index(psnr_max_value)
+index_ssim = ssim.index(ssim_max_value)
+
+
+plt.figure(file + ' Loss ')
+plt.title('Loss')
+plt.plot(vgg_loss, label="VGG loss")
+plt.plot(mse_loss, label="MSE loss")
+plt.plot(loss_min, linestyle = '--', label = str(loss_min_value))
+plt.legend()
+plt.ylabel('loss')
+plt.xlabel('epochs')
+
+
+plt.figure(file + ' PSNR and SSIM')
+plt.subplot(211)
+plt.title('PSNR')
+plt.plot(psnr, label='AVG PNSR')
+plt.plot(psnr_max, linestyle = '--', label = str(psnr_max_value))
+plt.plot(index_psnr, psnr_max_value, 'ro')
+plt.legend()
+plt.ylabel('PSNR')
+plt.xlabel('epochs')
+
+plt.subplot(212)
+plt.plot(ssim, label='AVG SSIM')
+plt.plot(ssim_max, linestyle = '--', label = str(ssim_max_value))
+plt.plot(index_ssim, ssim_max_value, 'ro')
+plt.legend()
+plt.ylabel('SSIM')
+plt.xlabel('epochs')
+
+plt.tight_layout()
+
+plt.show()
---
+++ plot_adv.py
@@ -0,0 +1,83 @@
+import argparse
+import csv
+import matplotlib.pyplot as plt
+
+# Plot settings
+parser = argparse.ArgumentParser(description='Neural Networks data plotter')
+parser.add_argument('-f', '--file', type=str, default='./logs/log.csv',
+					help='input file to plot (default:./logs/log.csv)')
+args = parser.parse_args()
+
+file = args.file
+vgg_loss = []
+mse_loss = []
+psnr = []
+ssim = []
+
+adv_D2x=[]
+adv_D4x=[]
+
+with open(file) as f:
+	reader = csv.reader(f)
+	for row in reader:
+		#print(row)
+		vgg_loss.append(row[1])
+		mse_loss.append(row[2])
+
+		adv_D4x.append(row[3])
+		adv_D2x.append(row[4])
+
+		psnr.append(row[5])
+		ssim.append(row[6])
+
+loss_min_value = min(vgg_loss)
+
+loss_min = [loss_min_value] * len(vgg_loss)
+
+psnr_max_value = max(psnr)
+
+psnr_max = [psnr_max_value] * len(psnr)
+
+ssim_max_value = max(ssim)
+
+ssim_max = [ssim_max_value] * len(ssim)
+
+index_psnr = psnr.index(psnr_max_value)
+index_ssim = ssim.index(ssim_max_value)
+
+
+plt.figure(file + " Adversarial Loss")
+plt.plot(adv_D4x, label="D4x loss")
+plt.plot(adv_D2x, label="D2x loss")
+plt.legend()
+plt.ylabel('loss')
+plt.xlabel('epochs')
+
+plt.figure(file + ' Loss and PSNR')
+plt.title('Loss')
+plt.plot(vgg_loss, label="VGG loss")
+plt.plot(mse_loss, label="MSE loss")
+plt.plot(loss_min, linestyle = '--', label = str(loss_min_value))
+plt.legend()
+plt.ylabel('loss')
+plt.xlabel('epochs')
+
+plt.figure(file + ' PSNR')
+plt.title('PSNR')
+plt.plot(psnr, label='AVG PNSR')
+plt.plot(psnr_max, linestyle = '--', label = str(psnr_max_value))
+plt.plot(index_psnr, psnr_max_value, 'ro')
+plt.legend()
+plt.ylabel('PSNR')
+plt.xlabel('epochs')
+
+plt.figure(file + ' SSIM')
+plt.title('SSIM')
+plt.plot(ssim, label='AVG SSIM')
+plt.plot(ssim_max, linestyle = '--', label = str(ssim_max_value))
+plt.plot(index_ssim, ssim_max_value, 'ro')
+plt.legend()
+plt.ylabel('SSIM')
+plt.xlabel('epochs')
+
+plt.show()
